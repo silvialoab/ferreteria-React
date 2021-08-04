c819651e4ab9cd2acc596895c53a5feab8382fa0
silvialoab <silvialoab.compu@gmail.com>
2021-08-04 20:20:02 -0300

    agregue titulo y algo de estilo

--- ferreteria-silviaabouaquin/src/App.js
+++ ferreteria-silviaabouaquin/src/App.js
@@ -1,12 +1,16 @@
 //import logo from './logo.svg';
 import './App.css';
-
 import NavBar from './Components/NavBar'
 
 function App() {
   return (
     <>
-       <NavBar></NavBar> 
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '10vh', backgroundColor:'cadetblue'}}>
+        <h1 >LOPEZ      Mayorista Ferretero</h1>
+      </div>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '20vh', backgroundColor:'cadetblue'}}>
+        <NavBar></NavBar>
+      </div>
     </>
   );
 }
---
+++ ferreteria-silviaabouaquin/src/Components/Titulo.jsx
@@ -0,0 +1,15 @@
+function Titulo(props) {
+
+    return(
+        <>
+       <Titulo>
+           <div>
+             <h1> LOPEZ </h1>   
+             <h3> Mayorista Ferretero </h3>  
+           </div> 
+       </Titulo> 
+    </>
+    )
+}
+
+
